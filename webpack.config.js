6771d3a152bad8be0e9a1d7567e4c206bdeb1fd6
Jessynt <xr5299@gmail.com>
2016-12-05 17:45:16 +0800

    :heavy_minus_sign: remove unused packages

--- webpack.config.js
+++ webpack.config.js
@@ -2,17 +2,20 @@ let webpack = require('webpack');
 let path = require('path');
 let AssetsPlugin = require('assets-webpack-plugin');
 let CleanPlugin = require('clean-webpack-plugin');
-
 let Production = process.env.NODE_ENV === 'production';
 
-let SOURCE_CODE_PATH = path.join(__dirname, './resources/assets/js');
+let JS_SOURCE_CODE_PATH = path.join(__dirname, './resources/assets/js');
+
 let DIST_PATH = path.join(__dirname, 'public/build');
 
 let Config = ({
     context: path.resolve('resources/assets'),
     entry: {
-        app: ['whatwg-fetch', SOURCE_CODE_PATH + '/home.js'],
-        admin: SOURCE_CODE_PATH + '/dashboard.js',
+        app: ['whatwg-fetch', JS_SOURCE_CODE_PATH + '/home.js'],
+        admin: [
+            JS_SOURCE_CODE_PATH + '/dashboard.js'
+        ]
+
     },
     output: {
         filename: 'js/[name]-[hash:8].js',
@@ -27,9 +30,9 @@ let Config = ({
         extensions: ['', '.js'],
         alias: {
             'vue$': 'vue/dist/vue',
-            'common': SOURCE_CODE_PATH + '/common',
-            'components': SOURCE_CODE_PATH + '/components',
-            'containers': SOURCE_CODE_PATH + '/containers',
+            'common': JS_SOURCE_CODE_PATH + '/common',
+            'components': JS_SOURCE_CODE_PATH + '/components',
+            'containers': JS_SOURCE_CODE_PATH + '/containers',
         },
     },
     module: {
@@ -38,7 +41,7 @@ let Config = ({
                 test: /\.js$/,
                 loader: 'babel',
                 exclude: /node_modules/,
-                include: SOURCE_CODE_PATH
+                include: JS_SOURCE_CODE_PATH
             },
             {
                 test: /\.vue$/,
@@ -53,7 +56,7 @@ let Config = ({
                 loader: 'html',
                 query: {interpolate: true},
                 exclude: /node_modules/,
-                include: SOURCE_CODE_PATH
+                include: JS_SOURCE_CODE_PATH
             },
         ]
     },
