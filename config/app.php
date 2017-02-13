765e76476d124de1856a3d7d2b52c5a83dd98c6c
Jessynt <xr5299@gmail.com>
2017-02-13 14:39:41 +0800

    upgrade to laravel 5.4

--- config/app.php
+++ config/app.php
@@ -166,6 +166,7 @@ return [
         /*
          * Package Service Providers...
          */
+        Laravel\Tinker\TinkerServiceProvider::class,
         Dingo\Api\Provider\LaravelServiceProvider::class,
         Tymon\JWTAuth\Providers\JWTAuthServiceProvider::class,
         Barryvdh\Debugbar\ServiceProvider::class,
---
+++ webpack.mix.js
@@ -0,0 +1,30 @@
+const {mix} = require('laravel-mix');
+/*
+ |--------------------------------------------------------------------------
+ | Elixir Asset Management
+ |--------------------------------------------------------------------------
+ |
+ | Elixir provides a clean, fluent API for defining some basic Gulp tasks
+ | for your Laravel application. By default, we are compiling the Sass
+ | file for our application, as well as publishing vendor resources.
+ |
+ */
+
+mix.copy(
+    "node_modules/font-awesome/fonts",
+    "public/fonts/font-awesome"
+);
+// less
+mix.less('resources/assets/less/admin.less', 'public/css/admin/admin.css');
+//sass
+mix.sass('resources/assets/sass/admin/dashboard.scss', 'public/css/admin')
+    .sass('resources/assets/sass/admin/plugins/editor.scss', 'public/css/admin');
+mix.combine(['public/css/admin/admin.css', 'public/css/admin/dashboard.css'], 'public/css/admin/all.css');
+mix.js('resources/assets/js/plugins/editor.js', 'public/js/admin')
+    .js('resources/assets/js/dashboard.js', 'public/js/admin');
+
+mix.sourceMaps();
+
+if (mix.config.inProduction) {
+    mix.version();
+}
